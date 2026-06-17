b5654154f582f6a3a30a3801b4a9e55e09de8518
Phuong Ng <phuongdc24v7k527@vlvh.ctu.edu.vn>
2026-06-17 11:43:20 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const cors = require("cors");
 
 const app = express();
 
+const contactsRouter = require("./app/routes/contact.route"); ////
+
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/contacts", contactsRouter); ////
+
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application."});
+  res.json({ message: "Welcome to contact book application." });
 });
 
-module.exports = app;
+module.exports = app; ////
